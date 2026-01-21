9cac3004fd8ec0dca94015fff3e00833ebd61bad
Jady Yang <jady@live.com>
2026-01-21 21:27:53 +0800

    fix: 把绝对路径改为相对路径

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -3227,7 +3227,7 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.18.19 fix: fix github action
 """
 
-from akshare._version import __version__
+from ._version import __version__
 __author__ = "AKFamily"
 
 """
